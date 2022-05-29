86d23e2da26bb4ae957f22ad03276a967e98a6d2
Garavirod <rodrigogarciaavila26@gmail.com>
2022-05-29 15:40:18 -0500

    se configuro el vue.config para exportacioes del build

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = defineConfig({
   ],
   publicPath:'/tendencys-test/',
   outputDir:'tendencys-test', 
-  lintOnSave:false
+  lintOnSave:false,
+  pwa:{
+    iconPaths: {
+      favicon32: "./public/favicon.png",
+      favicon16: "./public/favicon.png",
+      appleTouchIcon: "./public/favicon.png",
+      maskIcon: "./public/favicon.png",
+      msTileImage: "./public/favicon.png"
+    },  
+  }
 })
